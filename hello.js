f0637594c3ac3cfe3d456c5ff5a8384e0df52e26
jomicm <singularity.jmcm@gmail.com>
2019-08-27 23:32:59 +0000

    This is the functions file

---
+++ hello.js
@@ -0,0 +1,12 @@
+const sayHello = function() {
+  console.log('Hello, world');
+}
+sayHello();
+
+const sayHello2 = function(name){
+  console.log('Hello, ' + name);
+}
+
+sayHello2('Juanito');
+sayHello2('Mary');
+sayHello2('Miranda');
