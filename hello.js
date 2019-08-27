654971c93073398c23e5f350b80391ba415ee27f
jomicm <singularity.jmcm@gmail.com>
2019-08-27 23:36:40 +0000

    This is the functions file modified to represent the difference between console.log() and return

--- hello.js
+++ hello.js
@@ -10,3 +10,14 @@ const sayHello2 = function(name){
 sayHello2('Juanito');
 sayHello2('Mary');
 sayHello2('Miranda');
+
+const sayHelloToConsole = function (name) {
+  console.log('Hello, ' + name);
+}
+sayHelloToConsole('Johny');
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
