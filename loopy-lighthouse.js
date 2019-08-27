f111378f5ad95b845b0ff3d79e36c62ebc7df163
jomicm <singularity.jmcm@gmail.com>
2019-08-27 23:21:33 +0000

    This is the Loopy-Lighthouse evaluation modification

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,6 +1,6 @@
 for(let i = 100; i <= 200; i++){
   if(i % 12 === 0) {
-    console.log('LoopyLightHouse');
+    console.log('LoopyLighthouse');
   } else if (i % 4 === 0) {
     console.log('Lighthouse');
   } else if (i % 3 === 0) {
